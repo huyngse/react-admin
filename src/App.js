f7eb9f8f84fe830ad6d11977de4ef1a0bba4fa56
huyngse <ngogiahuy775309@gmail.com>
2023-12-15 20:52:34 +0700

    add topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 function App() {
   const [theme, colorMode] = useMode();
   return (
@@ -7,7 +8,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
--- src/theme.js
+++ src/theme.js
@@ -205,11 +205,6 @@ export const useMode = () => {
     }),
     []
   );
-  const theme = useMemo(
-    () => {
-      createTheme(themeSettings(mode))
-    },
-    [mode]
-  )
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return [theme, colorMode];
 };
