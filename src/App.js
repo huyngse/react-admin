972d83d3a2ac221cb4eff00780dec1ce635cd672
huyngse <ngogiahuy775309@gmail.com>
2023-12-17 15:18:05 +0700

    Add user profile form

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -31,7 +31,7 @@ function App() {
                 <Route path="/invoices" element={<Invoices/>}/>
                 <Route path="/contacts" element={<Contacts/>}/>
                 {/* <Route path="/bar" element={<Bar/>}/> */}
-                {/* <Route path="/form" element={<Form/>}/> */}
+                <Route path="/form" element={<Form/>}/>
                 {/* <Route path="/line" element={<Line/>}/> */}
                 {/* <Route path="/pie" element={<Pie/>}/> */}
                 {/* <Route path="/faq" element={<FAQ/>}/> */}
