c09cd828cb7efc0a0b8b916f1b5145cf466db5bf
huyngse <ngogiahuy775309@gmail.com>
2023-12-18 11:00:19 +0700

    Add FAQ page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
+// import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -30,13 +30,13 @@ function App() {
                 <Route path="/team" element={<Team />} />
                 <Route path="/invoices" element={<Invoices/>}/>
                 <Route path="/contacts" element={<Contacts/>}/>
-                {/* <Route path="/bar" element={<Bar/>}/> */}
                 <Route path="/form" element={<Form/>}/>
+                <Route path="/calendar" element={<Calendar/>}/>
+                <Route path="/faq" element={<FAQ/>}/>
+                {/* <Route path="/bar" element={<Bar/>}/> */}
                 {/* <Route path="/line" element={<Line/>}/> */}
                 {/* <Route path="/pie" element={<Pie/>}/> */}
-                {/* <Route path="/faq" element={<FAQ/>}/> */}
                 {/* <Route path="/geography" element={<Geography/>}/> */}
-                <Route path="/calendar" element={<Calendar/>}/>
               </Routes>
             </main>
         </div>
